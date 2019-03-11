8a84c7606d1d094b4809043f0ffb40b8bec841e3
Belyor <andrzejmakowksi94@gmail.com>
2019-03-11 17:44:09 +0100

    substraction added

--- main.cpp
+++ main.cpp
@@ -7,55 +7,50 @@ int main()
     string str_ulli_number, str_ulli_max;
     unsigned long long int ulli_number;
     vector <unsigned long long int> my_vec_number;
-    int size_ulli_max;
+    int size_str_ulli_number, size_ulli_max, size_str_ulli_substraction;
 
     //set max ullong_max total elements
-    str_ulli_max = to_string(111);
+    str_ulli_max = to_string(123);
     size_ulli_max = str_ulli_max.length();
 
     cout << "Gimmi numba\n";
     cin >> str_ulli_number;
-    cout <<"\n\n Number: "<<str_ulli_number<<endl;
+    cout <<"\n Number: "<<str_ulli_number<<endl;
+    size_str_ulli_number = str_ulli_number.length();
+    //size_str_ulli_substraction = size_str_ulli_number;
 
-    if (str_ulli_number.length()>size_ulli_max)
+    if (size_str_ulli_number>size_ulli_max)
     {
         int p = 0;
         my_vec_number.push_back(1);
         string str_lower_ulli_number;
         reverse(str_ulli_number.begin(),str_ulli_number.end());
+
         reverse(str_ulli_max.begin(),str_ulli_max.end());
-        cout <<"\n\n Lower number: "<<str_lower_ulli_number<<endl;
+        str_ulli_max.resize(size_str_ulli_number,'0');
 
         //resizing strings to longer one
-        str_ulli_max.resize(str_ulli_number.length(),'0');
-        str_lower_ulli_number.resize(str_ulli_number.length(),'0');
+        str_lower_ulli_number.resize(size_str_ulli_number,'0');
         cout <<"\n\n Lower number resize: "<<str_lower_ulli_number<<endl;
-
-        int len = str_ulli_number.length();
-        for (int i = 0; i<len; i++)
+        for (int i = 0; i<size_str_ulli_number; i++)
         {
-            str_lower_ulli_number[i] = str_ulli_number[i] - str_ulli_max[i] + '0';
-            cout<<"lower number "<<str_lower_ulli_number<<"   "<<i <<" th operation"<<endl;
+            str_lower_ulli_number[i] = str_ulli_number[i] - p - str_ulli_max[i] + '0';
+            p=0;
             if (str_lower_ulli_number[i]<'0')
             {
-                str_ulli_number[i+1]-=1;
-                str_ulli_number[i]+=10;
-                str_lower_ulli_number[i] = str_ulli_number[i] - str_ulli_max[i] + '0';
+                str_lower_ulli_number[i]+=10;
+                p=1;
             }
-            // reverse(str_ulli_number.begin(),str_ulli_number.end());
-            cout<<str_lower_ulli_number << "\n" << str_ulli_number;
+            cout<<"number "<<str_lower_ulli_number<<"   "<<i <<" th operation"<<endl;
         }
-
-        for (int j =0; j<str_ulli_number.length(); j++)
+        for (int j=0 ; j<size_str_ulli_number; j++)
         {
-            str_ulli_number[j]=str_lower_ulli_number[j];
-           // cout<<str_lower_ulli_number[j] << "\t" << str_ulli_number[j] << "\n";
-
+            str_ulli_number[j] = str_lower_ulli_number[j];
         }
         reverse(str_ulli_number.begin(), str_ulli_number.end());
         cout << "Wynik: " << str_ulli_number << endl;
     }
-    else if (str_ulli_number.length()<size_ulli_max)
+    else if (size_str_ulli_number<size_ulli_max)
     {
         ulli_number = stoull(str_ulli_number);
         my_vec_number.push_back(0);
@@ -66,6 +61,11 @@ int main()
             cout<<my_vec_number[i];
         }
     }
+    else if (size_str_ulli_number==size_ulli_max)
+    {
+
+    }
+
 
     return 0;
 }
